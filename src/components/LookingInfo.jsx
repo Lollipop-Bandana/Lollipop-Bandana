2ee43c6a0a32c779ee29a1d556d42471f3ea5ca1
Hack Reactor Students <accounts@hackreactor.com>
2016-10-20 15:50:28 -0700

    Create component for additional renter information

---
+++ src/components/LookingInfo.jsx
@@ -0,0 +1,12 @@
+// extension of user profile for user that is looking for a room
+
+var React = require('react');
+
+var LookingInfo = (props) = {
+  return (
+    <div className="looking-info">
+      <div className="room-type">Type of Room: {props.form.additional.type}</div><br />
+      <div className="room-price">Price: {props.form.additional.minimumPrice} - {props.form.additional.maximumPrice}</div>
+    </div>
+  );
+}
