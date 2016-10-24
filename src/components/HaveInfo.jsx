1840c8386ff9a902962e1021969ba103a101494f
Marissa <marissaokada@gmail.com>
2016-10-24 12:11:43 -0700

    Fix syntax for existing components

--- src/components/HaveInfo.jsx
+++ src/components/HaveInfo.jsx
@@ -2,12 +2,10 @@
 
 var React = require('react');
 
-var HaveInfo = (props) = {
-  return (
-    <div className="have-info">
-      <div className="address">Address: {props.form.additional.address}</div><br />
-      <div className="room-type">Type of Room: {props.form.additional.type}</div><br />
-      <div className="room-price">Price: {props.form.additional.price}</div>
-    </div>
-    )
-}
+var HaveInfo = (props) => (
+  <div className="have-info">
+    <div className="address">Address: {props.form.additional.address}</div><br />
+    <div className="room-type">Type of Room: {props.form.additional.type}</div><br />
+    <div className="room-price">Price: {props.form.additional.price}</div>
+  </div>
+);
--- src/components/Personality.jsx
+++ src/components/Personality.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
 
-var Personality = () => {
-  
-}
+var Personality = () => (
+
+);
