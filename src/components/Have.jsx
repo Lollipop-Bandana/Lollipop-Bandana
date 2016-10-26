d66757e119d09aebdc2317c039da411518663929
Marissa <marissaokada@gmail.com>
2016-10-25 17:26:09 -0700

    (feat) Alter additional looking/having forms

--- src/components/Have.jsx
+++ src/components/Have.jsx
@@ -1,27 +1,28 @@
 // have room form
 var Have = () => (
-  <div className="have-info">Address: 
+  <div className="have-info"><h3>Address: </h3>
     <form className="address-form">
+      Address 1:<br />
       <input className="address1" name="address1" required placeholder="Address 1" /><br />
+      Address 2:<br />
       <input className="address2" name="address2" placeholder="Address 2" /><br />
+      City:<br />
       <input className="city" type="text" name="city" required placeholder="City" /><br />
+      State:<br />
       <input className="state" type="text" name="state" required placeholder="State" /><br />
+      Zipcode:<br />
       <input className="zipcode" name="zipcode" required placeholder="Zipcode" /><br />
     </form>
     <br />
-    <div className="room-type">
-      <div className="room dropdown">
-        <button className="dropdown-toggle" type="button" dataToggle="dropdown">Room Type:
-        <span class="caret"></span></button>
-        <ul class="dropdown-menu">
-          <li>Shared Room</li>
-          <li>One Bedroom</li>
-        </ul>
-      </div>
-    </div>
-    <br />
-    <div className="price">Price:
-      <input className="room-price" required placeholder="Enter Amount"/>
+    <h3>Room Type:</h3>
+    <select name="room-type">
+      <option value="share-room">Shared Room</option>
+      <option value="one-bedroom">One Bedroom</option>
+    </select>
+    <br /><br />
+    <div className="price">
+      <h3>Price:</h3>
+      $<input className="room-price" type="number" required placeholder="Enter Amount"/>
     </div>
     </div>
 );
