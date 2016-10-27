aa1e02312fe5f74d8475a6a9121aa355ed20f424
mjstromberg <michaeljstromberg@gmail.com>
2016-10-27 00:14:04 -0700

    Remove body-parser from server index.js, unnecessary at the moment

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var roomRouter = require('./routers/room.js');
 var roomyRouter = require('./routers/roomy.js');
 
@@ -10,7 +9,6 @@ var app = express();
 
 // use middleware here
 app.use(express.static(__dirname + '/../src'));
-app.use(bodyParser.json());
 app.use('/api/room', roomRouter); // adjust router name and endpoint when necessary
 app.use('/api/roomy', roomyRouter); // adjust router name and endpoint when necessary
 
