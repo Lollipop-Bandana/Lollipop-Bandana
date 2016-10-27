11bd4b7b30d7c2ad819e5b45f1b0a832ffe0f0d6
mjstromberg <michaeljstromberg@gmail.com>
2016-10-27 00:04:54 -0700

    Update server index.js to require routers for room and roomy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// require routers here
+var roomRouter = require('./routers/room.js');
+var roomyRouter = require('./routers/roomy.js');
+
 
 var port = 8000;
-
 var app = express();
+
 
 // use middleware here
 app.use(express.static(__dirname + '/../src'));
+app.use(bodyParser.json());
+app.use('/api/room', roomRouter); // adjust router name and endpoint when necessary
+app.use('/api/roomy', roomyRouter); // adjust router name and endpoint when necessary
 
 
 app.listen(port, function() {
