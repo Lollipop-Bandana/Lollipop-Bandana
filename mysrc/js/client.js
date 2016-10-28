ec13775c23909172807b3950cc4b37631479f3b6
Shensen Wang <shendude@hotmail.com>
2016-10-27 21:11:06 -0700

    prototype framework

--- mysrc/js/client.js
+++ mysrc/js/client.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Layout from "./components/Layout";
+import Layout from "./components/Layout.jsx";
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
