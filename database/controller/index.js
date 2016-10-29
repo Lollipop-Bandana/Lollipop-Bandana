55251ee92e3ddeee586756f85588cd4c2aa1de6d
Marissa <marissaokada@gmail.com>
2016-10-28 17:57:19 -0700

    (refactor) Finish user post to database

--- database/controller/index.js
+++ database/controller/index.js
@@ -19,6 +19,20 @@ module.exports = {
         // (req.body.birthmonth + "/" + req.body.birthday + "/" + req.body.birthyear)
         gender: req.body.gender,
         aboutme: req.body.aboutme,
+        quiz1: req.body.quiz1,
+        quiz2: req.body.quiz2,
+        quiz3: req.body.quiz3,
+        quiz4: req.body.quiz4,
+        quiz5: req.body.quiz5,
+        quiz6: req.body.quiz6,
+        quiz7: req.body.quiz7,
+        quiz8: req.body.quiz8,
+        quiz9: req.body.quiz9,
+        quiz10: req.body.quiz10,
+        profilepicture: req.body.profilepicture,
+        totalfriends: req.body.totalfriends,
+        looking: req.body.looking,
+        have: req.body.have
         }})
         .spread(function(user, created) {
           res.sendStatus(created ? 201 : 200);
