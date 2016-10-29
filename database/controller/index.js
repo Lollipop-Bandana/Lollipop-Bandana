521ae5b2c02a309e33dd984317e00f566a9226e5
Marissa <marissaokada@gmail.com>
2016-10-28 17:54:35 -0700

    (refactor) Add more to controller

--- database/controller/index.js
+++ database/controller/index.js
@@ -12,7 +12,13 @@ module.exports = {
       // if using only id, we can change this to req.body.id
       db.User.findOrCreate({where: {
         id: req.body.id,
-        username: req.body.username
+        username: req.body.username,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        birthday: req.body.birthday,
+        // (req.body.birthmonth + "/" + req.body.birthday + "/" + req.body.birthyear)
+        gender: req.body.gender,
+        aboutme: req.body.aboutme,
         }})
         .spread(function(user, created) {
           res.sendStatus(created ? 201 : 200);
