823efd755fe39fa4c03dd28b520a5f33a9767765
Shensen Wang <shendude@hotmail.com>
2016-10-29 18:19:06 -0700

    mysrc bugfixes

--- mysrc/js/components/Homepage.jsx
+++ mysrc/js/components/Homepage.jsx
@@ -4,8 +4,7 @@ class Homepage extends React.Component {
   render() {
     return (
       <div>
-        <a href="/auth/facebook"><button>Login</button></a>
-        <a href="/auth/logout"><button>Logout</button></a>
+        Welcome to Roomy!
       </div>
     );
   }
